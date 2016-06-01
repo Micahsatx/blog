592e01d72733a5f4a2b2c175ebd01549fbb43951
micahbohemian <Micahjohnson1983@gmail.com>
2016-06-01 11:17:28 -0500

    exercise 10.1.1

---
+++ app/views/my-first-view.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>My First View</title>
+</head>
+<body>
+    <h1>Hello, Codeup!</h1>
+    <p>The number rolled was: <?= $name; ?> !</p>
+</body>
+</html>
