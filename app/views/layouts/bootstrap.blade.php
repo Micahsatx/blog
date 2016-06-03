50151a18768100697ca56d7a7cb4dce00b3db3d7
micahbohemian <Micahjohnson1983@gmail.com>
2016-06-03 15:25:41 -0500

    made a second master layout.  didnt end up using this one

---
+++ app/views/layouts/bootstrap.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- copy and pasted from twitter bootstrap getting started -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    
+    <!-- ***my own style sheet*** -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    
+    <!-- This is the link for the font awesome icon i downloaded that is my pretend logo -->
+    <link rel="stylesheet" href="/css/resume.css"> 
+    
+    
+    <!-- Latest compiled and minified JavaScript -->
+    <!-- javascript script from twitter bootstrap getting started.  copy / paste -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+
+    @yield('top-script')
+
+    <title>Micah's Blog</title>
+</head>
+<body>
+    <div class="container">
+    @yield('content')
+    </div>
+    
+    <!-- j query library hosted by google...google it. copy.paste. -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    @yield('bottom-script')
+</body>
+</html>
