a8edaf5324965151c067cd8a29b8ca292bab5317
micahbohemian <Micahjohnson1983@gmail.com>
2016-06-06 16:11:59 -0500

    added in error / success message for when a user creates a new add

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -23,6 +23,14 @@
 </head>
 <body>
     @include('layouts.navbar')
+    
+    @if (Session::has('successMessage'))
+        <div class="alert alert-success">{{{ Session::get('successMessage') }}}</div>
+    @endif
+    @if (Session::has('errorMessage'))
+        <div class="alert alert-danger">{{{ Session::get('errorMessage') }}}</div>
+    @endif
+    
     @yield('content')
 
     
