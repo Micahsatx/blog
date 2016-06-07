05c98623a26d769e08b47d8f81c98375e8ed3ed8
micahbohemian <Micahjohnson1983@gmail.com>
2016-06-07 15:59:37 -0500

    moved divs around so the pagination was at the bottom

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -3,28 +3,40 @@
 @section('content')
 
     <h1>All Posts</h1>
-    <div>
     @foreach ($posts as $post)
-        <div class="container">
-            <h3><a href="{{{ action('PostsController@show', $post->id) }}}">{{{ $post->title }}}</a></h3>
-            <p><small>Posted on:
-                {{ 
-                    $post
-                    ->created_at
-                    ->setTimezone('America/Chicago')
-                    ->format('l, F jS Y @ h:i:s A') 
-                }}
-            </small></p>    
-        </div> 
-
+    <div class="postsRow">
+        <div class="col-md-8 col-md-offset-2 col-sm-12">
+            <div class="card-post">
+                <div class= "unused">
+                    <h3><a href="{{{ action('PostsController@show', $post->id) }}}">{{{ $post->title }}}</a></h3>
+                    
+                    <p>{{{ $post->content }}}</p>
+                    <p><small>Posted on:
+                        {{ 
+                            $post
+                            ->created_at
+                            ->setTimezone('America/Chicago')
+                            ->format('l, F jS Y @ h:i:s A') 
+                        }}
+                    </small></p>
+                    <!-- on the specific post it shows the who wrote the post -->
+                    <p><small>
+                        Written by: {{{ $post->user->user}}}
+                    </small></p>    
+                </div> 
+            </div>
+        
+        </div>
+    </div>
     @endforeach  
 
-        <div class="container">
+    <div class="row col-md-8 col-md-offset-2 col-sm-12">            
         <!-- the bar at the bottom of blog.dev/posts that paginates/nextpage -->
             {{ $posts->links() }}
         <!-- you can put in info like page 2 of 41 -->
-        <!-- look at pagination docs at the different options -->
-        </div>
-    </div>    
-@stop      
+        <!-- look at pagination docs at the different options -->  
+    </div>
+@stop  
+
+
     
