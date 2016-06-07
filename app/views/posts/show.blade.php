db18e1d6b4c3477f2d55b47a3c067de78215e6f6
micahbohemian <Micahjohnson1983@gmail.com>
2016-06-07 16:00:42 -0500

    attemtpting to make it so you only see the edit post when logged in.

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -7,11 +7,18 @@
     <title>Index</title>
 </head>
 <body>
-    <h1>title: {{{ $post->title }}}</h1>
-<!-- on the specific post it shows the who wrote the post -->
-    <p>Written by: {{{ $post->user->user}}}</p>
-    <p>{{{ $post->content}}}</p>
-    <a href="{{{ action('PostsController@edit', $post->id) }}}">Edit this entry</a>
+    <div class="individualPost">
+        <div class="col-md-10 col-md-offset-1 col-sm-12">
+
+            <h1>{{ $post->title }}</h1>
+        <!-- on the specific post it shows the who wrote the post -->
+            <p>Written by: {{{ $post->user->user}}}</p>
+            <p>{{ $post->content}}</p>
+            @if(Auth::check())
+            <a href="{{{ action('PostsController@edit', $post->id) }}}">Edit this entry</a>
+            @endif
+        </div>    
+    </div>
 </body>
 </html>
 @stop
