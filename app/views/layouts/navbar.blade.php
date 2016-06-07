789455322641365a36b7a69ef97abc369909afcc
micahbohemian <Micahjohnson1983@gmail.com>
2016-06-07 15:56:55 -0500

    navbar features fully functional.  if / else put in so user only sees certain links when logged in / logged out

--- app/views/layouts/navbar.blade.php
+++ app/views/layouts/navbar.blade.php
@@ -8,18 +8,23 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" onclick="location.href='https://github.com/Micahsatx/blog';" href="#"><i class="fa fa-gamepad fa-2x"></i> My Logo</a>
-    </div>
+      <a class="navbar-brand" onclick="location.href='https://github.com/Micahsatx/blog';" href="#"><i class="fa fa-gamepad fa-2x"></i> MicahSATX</a>
+{{--       <small>Welcome: {{Auth::user}}</small>
+ --}}    </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse mynav" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="{{{ action('PostsController@index') }}}">Show All Posts</a></li>
-        <li><a href="{{{ action('PostsController@create') }}}">New Blog Entry</a></li>
-        <li><a href="">About Me</a></li>
-        <button type="button" class="btn btn-default" onclick="location.href='SEARCH PAGE';">Search</button>
-        <button type="button" class="btn btn-default" onclick="location.href='NEED A LOGIN USER PAGE';">Login</button>
-        <button type="button" class="btn btn-default" onclick="location.href='NEED A CREATE USER PAGE';">Create User</button>
+        @if(Auth::check())
+          <li><a href="{{{ action('PostsController@index') }}}">Show All Posts</a></li>
+          <li><a href="{{{ action('PostsController@create') }}}">New Blog Entry</a></li>
+          <li><a href="{{{ action('HomeController@doLogout') }}}">Logout</a></li>
+        @else
+          <li><a href="{{{ action('PostsController@index') }}}">Show All Posts</a></li>
+          <li><a href="{{{ action('HomeController@makeNewUserPage') }}}">Create New User</a></li>
+          <li><a href="{{{ action('HomeController@loginForm') }}}">Login</a></li>
+        {{-- <li><a href="{{{ }}}">Create New User</a></li> --}}
+        @endif
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
