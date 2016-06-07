5088b856dbe032df5aca6d39ee303d42a2ab1efb
micahbohemian <Micahjohnson1983@gmail.com>
2016-06-07 16:00:13 -0500

    login page created and functional

--- app/views/posts/login.blade.php
+++ app/views/posts/login.blade.php
@@ -4,12 +4,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Index</title>
+    <title>Login</title>
 </head>
 <body>
-    <h1>title: {{{ $post->title }}}</h1>
-    <p>{{{ $post->content}}}</p>
-    <a href="{{{ action('PostsController@edit', $post->id) }}}">Edit this entry</a>
+  <section class="container">
+    <div class="login">
+      <h1>Login to Blog</h1>
+      <form method="post" action="{{{ action('HomeController@doLogin')}}}">
+        {{ Form::token() }}
+        <p><input type="text" name="email" value="" placeholder="Email"></p>
+        <p><input type="password" name="password" value="" placeholder="Password"></p>
+        <p class="submit"><input type="submit" name="commit" value="Login"></p>
+      </form>
+    </div>
 </body>
 </html>
 @stop
