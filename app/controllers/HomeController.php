5d1711d54baa26757e0ee12481d8a5d52450f719
micahbohemian <Micahjohnson1983@gmail.com>
2016-06-07 15:54:20 -0500

    login, logout, create user methods created.  everthing function (except image uploader)

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -57,4 +57,61 @@ class HomeController extends BaseController {
 	    return View::make('roll-dice')->with($data);
 	}
 
+	public function loginForm()
+	{
+		// get is the routes.php  it gets loginForm
+		// go to posts/login
+		return View::make('posts.login');
+	}
+
+	public function doLogin()
+	{
+		// validator checks all the fields against the rules set up in User class
+		// (making sure email is unique)
+
+		$rules = [
+			'email' => 'required',
+			'password' => 'required'
+		];
+
+		$validator = Validator::make(Input::all(), $rules);
+
+		// attempt validation
+	    if ($validator->fails()) {
+	        // validation failed, redirect to the login page with validation errors and old inputs	        
+			Session::flash('errorMessage', 'Login failed due to inccorect credentials' );
+	        return Redirect::back()->withErrors($validator);
+	    } else {
+			// if there are no validation errors continue on.
+			// get the email and password from the form
+			$data = [
+				'email' => Input::get('email'),
+				'password' => Input::get('password'),
+			];
+	
+			// if email and password match send user to the posts page
+			if (Auth::attempt($data)) {
+	    		return Redirect::action('PostsController@index');
+			} else {
+			    // login failed, go back to the login screen
+				Session::flash('errorMessage', 'auth::atempt failed' );
+				return Redirect::back()->withInput();
+			}
+	    }
+
+
+	}
+
+	public function doLogout()
+	{
+		Auth::logout();
+		return Redirect::action('PostsController@index');
+	}
+
+	public function makeNewUserPage()
+	{
+		// dd('making it to homecontroller');
+		return View::make('posts.newUser');
+	}
+
 }
