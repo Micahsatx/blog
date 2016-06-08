36839b11c6a31dda4313947b7e71aadae9352226
micahbohemian <Micahjohnson1983@gmail.com>
2016-06-08 10:56:14 -0500

    added divs/classes to uniform css styling

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -9,16 +9,20 @@
 <body>
     <div class="individualPost">
         <div class="col-md-10 col-md-offset-1 col-sm-12">
+            <div class= "postsRow">
+                <h1>{{ $post->title }}</h1>
+            <!-- on the specific post it shows the who wrote the post -->
+                <p>Written by: {{{ $post->user->user}}}</p>
+                <p>{{ $post->content}}</p>
+                <img class= "img-responsive" src="{{ $post->img }}">
+                <a href="{{{ action('PostsController@index') }}}">Go back <br></a>
 
-            <h1>{{ $post->title }}</h1>
-        <!-- on the specific post it shows the who wrote the post -->
-            <p>Written by: {{{ $post->user->user}}}</p>
-            <p>{{ $post->content}}</p>
-            @if(Auth::check())
-                @if($post->user_id == Auth::id())
-            <a href="{{{ action('PostsController@edit', $post->id) }}}">Edit/Delete this entry</a>
+                @if(Auth::check())
+                    @if($post->user_id == Auth::id())
+                <a href="{{{ action('PostsController@edit', $post->id) }}}">Edit/Delete this entry</a>
+                    @endif
                 @endif
-            @endif
+            </div>    
         </div>    
     </div>
 </body>
