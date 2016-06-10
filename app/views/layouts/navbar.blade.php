16463fc1d770fec66201a98d70cf4c82928006c8
micahbohemian <Micahjohnson1983@gmail.com>
2016-06-10 10:02:27 -0500

    cleaned up code and added notes

--- app/views/layouts/navbar.blade.php
+++ app/views/layouts/navbar.blade.php
@@ -8,17 +8,20 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
+        {{-- icon/link on navbar that takes user to my github page --}}
         <a class="navbar-brand" onclick="location.href='https://github.com/Micahsatx/blog';" href="#"><i class="fa fa-gamepad fa-2x"></i> MicahSATX</a>
     </div>
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
+    <!-- Links on navbar. -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav navbar-right">
+           {{-- if the user is logged on they see these 3 links, else they see those 3 --}}
             @if(Auth::check())
                 <li><a href="{{{ action('PostsController@index') }}}">Show All Posts</a></li>
                 <li><a href="{{{ action('PostsController@create') }}}">New Blog Entry</a></li>
                 <li><a href="{{{ action('HomeController@doLogout') }}}">Logout</a></li>
             @else
+                {{-- links seen when NOT logged in --}}
                 <li><a href="{{{ action('PostsController@index') }}}">Show All Posts</a></li>
                 <li><a href="{{{ action('HomeController@makeNewUserPage') }}}">Create New User</a></li>
                 <li><a href="{{{ action('HomeController@loginForm') }}}">Login</a></li>
