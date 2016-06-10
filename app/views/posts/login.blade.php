826d05e18ef46d2ea996212bc690441d9dd6c9b7
micahbohemian <Micahjohnson1983@gmail.com>
2016-06-10 11:22:23 -0500

    addings notes concerning csrf and {{{}}}.

--- app/views/posts/login.blade.php
+++ app/views/posts/login.blade.php
@@ -10,7 +10,9 @@
   <section class="container">
     <div class="login">
       <h1>Login to Blog</h1>
+      {{-- tripple {{{}}} strip tags so that code injection isnt as likely to occur.  one of several layers of protection to keep user from submitting malicious code through the form itself --}}
       <form method="post" action="{{{ action('HomeController@doLogin')}}}">
+        {{-- the token is what helps protect against csrf attacks.. cross site request forgery.  bots / users submitting malicious information through the URL or without ever actually visiting the form.  this is a second layer of protection --}}
         {{ Form::token() }}
         <p><input type="text" name="email" value="" placeholder="Email"></p>
         <p><input type="password" name="password" value="" placeholder="Password"></p>
