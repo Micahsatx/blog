9f495ec0620c50a0a50ce7d782e00ace6b35f57a
micahbohemian <Micahjohnson1983@gmail.com>
2016-06-10 09:59:44 -0500

    cleaned up code and added notes

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -2,7 +2,9 @@
 <html lang="en">
 <head>
     {{-- stackover flow solution to  navbar collapse --}}
+    {{-- found in bootstrap "issues" --}}
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    {{-- script for text markdown editor found in create/edit post --}}
     <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
     
     <!-- copy and pasted from twitter bootstrap getting started -->
@@ -17,7 +19,7 @@
     
     {{-- my css --}}
     <link rel="stylesheet" href="/css/blog.css"> 
-
+    {{-- icon for browser tab at the top --}}
     <link rel="icon" href="/favicon.ico"> 
     
     
@@ -30,23 +32,27 @@
 </head>
 <body>
     @include('layouts.navbar')
-    
+    {{-- location of alert messages for when user succesfully logins in --}}
     @if (Session::has('successMessage'))
         <div class="alert alert-success">{{{ Session::get('successMessage') }}}</div>
     @endif
+    {{-- location of alert messages for when user fails login --}}
+
     @if (Session::has('errorMessage'))
         <div class="alert alert-danger">{{{ Session::get('errorMessage') }}}</div>
     @endif
-    
+    {{-- location of content in page being viewed --}}
     @yield('content')
 
     
     <!-- j query library hosted by google...google it. copy.paste. -->
+    {{-- dont forget jquery needs to be above javascript! --}}
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 
     <!-- Latest compiled and minified JavaScript -->
     <!-- javascript script from twitter bootstrap getting started.  copy / paste -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    {{-- location of footer information.. not used on this site --}}
     @yield('bottom-script')
 </body>
 </html>
