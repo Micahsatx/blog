c9e74ebff862696a3f4eb3bbd27d64ee0b33ab97
micahbohemian <Micahjohnson1983@gmail.com>
2016-06-10 09:41:29 -0500

    cleaning up code

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -28,32 +28,7 @@
             </div>
         </div>
     </div>
-    {{-- <div class="">
-        <div class="hidden-xl hidden-lg hidden-md col-sm-12 col-xs-12">
-            <div class="card-post">
-                <div class= "smallPostsRow">
-                    <h3><a href="{{{ action('PostsController@show', $post->id) }}}">{{ $post->title }}</a></h3>
-                    
-                    <p>{{ $post->content }}</p>
-                    <img class= "img-responsive" src="{{ $post->img }}">
-                    <p><small>Posted on:
-                        {{ 
-                            $post
-                            ->created_at
-                            ->setTimezone('America/Chicago')
-                            ->format('l, F jS Y @ h:i:s A') 
-                        }}
-                    </small></p>
-                    <!-- on the specific post it shows the who wrote the post -->
-                    <p><small>
-                        Written by: {{{ $post->user->user}}}
-                    </small></p>    
-                </div> 
-            </div>
-        </div>
-    </div> --}}
     @endforeach 
-
 
     <div class="row col-md-8 col-md-offset-2 col-sm-12">            
         <!-- the bar at the bottom of blog.dev/posts that paginates/nextpage -->
